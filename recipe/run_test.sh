304a42c13e7966913403913d241db904cccac947
Mervin Fansler <fanslerm@mskcc.org>
2023-10-31 16:28:29 -0400

    Delete recipe/run_test.sh

--- recipe/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## import test
-${R} -e "library('RMariaDB')"
-
-## testthat tests
-cp ${RECIPE_DIR}/test.cnf ~/.my.cnf
-$R -e "testthat::test_file('tests/testthat.R', stop_on_failure=TRUE)"
